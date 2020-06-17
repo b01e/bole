821656097f44ffcd72dbccf98b7e795f89ab0fc5
mixindagege <54098691+mixindagege@users.noreply.github.com>
2020-06-17 14:54:09 +0800

    Update 全国建筑市场监管公共服务平台.py

--- 全国建筑市场监管公共服务平台.py
+++ 全国建筑市场监管公共服务平台.py
@@ -12,14 +12,12 @@ print(resp.text)
 
 class AESDecrypt:
     iv = '0123456789ABCDEF'
-    key = 'jo8j9wGw%6HbxfFn'
+    k = 'jo8j9wGw%6HbxfFn'
 
     @classmethod
-    def _pkcs7unpadding(cls, text):
+    def PKCS7Padding(cls, text):
         """
-        处理使用PKCS7填充过的数据
-        :param text: 解密后的字符串
-        :return:
+        默认即为Pkcs7填充
         """
         length = len(text)
         unpadding = ord(text[length - 1])
@@ -27,17 +25,12 @@ class AESDecrypt:
 
     @classmethod
     def decrypt(cls, content):
-        """
-        AES解密，模式cbc，去填充pkcs7
-        :param content: 16进制编码的加密字符串
-        :return: 返回解密后的字符串
-        """
-        key = bytes(cls.key, encoding='utf-8')
+        key = bytes(cls.k, encoding='utf-8')
         iv = bytes(cls.iv, encoding='utf-8')
         cipher = AES.new(key, AES.MODE_CBC, iv)
         decrypt_bytes = cipher.decrypt(bytes.fromhex(content))
         result = str(decrypt_bytes, encoding='utf-8')
-        result = cls._pkcs7unpadding(result)
+        result = cls.PKCS7Padding(result)
         return result
 
 
